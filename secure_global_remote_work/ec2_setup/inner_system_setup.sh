254f75ece783e667a9a077574f2ce69b08bd8ecf
w-ryan-jung <w.ryan.jung@gmail.com>
2023-09-18 21:14:39 +0900

    update

--- secure_global_remote_work/ec2_setup/inner_system_setup.sh
+++ secure_global_remote_work/ec2_setup/inner_system_setup.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo "p@ssw0rd" | passwd --stdin root
-sed -i "s/^PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-sed -i "s/^#PermitRootLogin yes/PermitRootLogin yes/g" /etc/ssh/sshd_config
-systemctl restart sshd
-
 yum install -y httpd
 systemctl start httpd & systemctl enable httpd
-
 echo "<h1>Welcome!! Inner System Portal</h1>" > /var/www/html/index.html
