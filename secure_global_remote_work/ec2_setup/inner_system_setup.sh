3c78025ac6a22c09d3261bd0d3218676dd36683c
w-ryan-jung <w.ryan.jung@gmail.com>
2023-09-20 10:16:05 +0900

    update

--- secure_global_remote_work/ec2_setup/inner_system_setup.sh
+++ secure_global_remote_work/ec2_setup/inner_system_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install -y httpd
 systemctl start httpd & systemctl enable httpd
-wget -O /var/www/html/index.html https://raw.githubusercontent.com/w-ryan-jung/AWSCloudFormation/main/secure_global_remote_work/ec2_setup/index.html 
+wget -O /var/www/html/index.html https://raw.githubusercontent.com/w-ryan-jung/AWSCloudFormation/main/secure_global_remote_work/ec2_setup/index.html
 chown apache.root /var/www/html/index.html
